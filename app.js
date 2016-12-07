65f1eac1a874f39d8ae153d02ca9897a6f7137a3
Noah Huber-Feely <nhuberfeely@gmail.com>
2016-12-07 11:50:11 -0600

    Adding needed function call and drinking another cup of coffee to clear the brain fog

--- app.js
+++ app.js
@@ -123,6 +123,7 @@ jobApp.intent("JobRandom",
     var item = Math.floor(Math.random() * JOB_NAMES.length);
     getCareer(JOB_NAMES[item]).then((data) => {
       response.say('Rolling the dice... And ' + JOB_NAMES[item] + ' has been selected. Here is some info. ' + data.description);
+      response.send();
     });
     return false;
   }
